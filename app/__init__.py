e911f976424366c1cc05c09d0c17d08a507c54d3
admin <alexandrpeks@gmail.com>
2024-08-21 21:13:23 +0400

    add folder templates and create base.html and index.html

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from .extensions import db
 from .config import Config
 from .routes.user import user
 from .routes.post import post
+from .routes.main import main
 
 
 def create_app(config_class=Config):
@@ -13,6 +14,7 @@ def create_app(config_class=Config):
 
     app.register_blueprint(user)
     app.register_blueprint(post)
+    app.register_blueprint(main)
 
     db.init_app(app)
     with app.app_context():
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from ..extensions import db
 from ..models.user import User
 
@@ -6,6 +6,6 @@ from ..models.user import User
 user = Blueprint('user', __name__)
 
 
-@user.route('/')
-def index():
-    return 'User API'
+@user.route('/user')
+def get_user():
+    return 'Look'
